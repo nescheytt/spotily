8481a33a98d1c1bbdb48e34794e773e03fed871d
nescheytt <nahuel.scheytt@gmail.com>
2023-09-23 13:42:52 +0100

    feat: initial project

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "tw"],
+}
