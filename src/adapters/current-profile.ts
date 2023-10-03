db6a8fd45459f287189fb93d690a667dcc0ba5b8
nescheytt <nahuel.scheytt@gmail.com>
2023-10-03 02:14:12 +0100

    fix: invalid type

--- src/adapters/current-profile.ts
+++ src/adapters/current-profile.ts
@@ -6,11 +6,15 @@ import {
 export function createAdaptedCurrentProfile(
   profile: EndpointCurrentProfile
 ): CurrentProfile {
+  const totalFollowers = new Intl.NumberFormat("de-DE").format(
+    profile.followers.total
+  )
+
   const formattedCurrentProfile: CurrentProfile = {
     name: profile.display_name,
     url: profile.external_urls.spotify,
     cover: profile.images[1].url,
-    followers: profile.followers.total,
+    followers: totalFollowers,
     country: profile.country,
     product: profile.product,
   }
--- src/adapters/following.ts
+++ src/adapters/following.ts
@@ -4,16 +4,20 @@ export function createAdaptedFollowing(
   following: EndpointFollowing[]
 ): Following[] {
   const formattedFollowing: Following[] = following.map(
-    (follower: EndpointFollowing) => ({
-      name: follower.name,
-      url: follower.external_urls.spotify,
-      cover: follower.images[1].url,
-      id: follower.id,
-      popularity: follower.popularity,
-      followers: new Intl.NumberFormat("de-DE").format(
+    (follower: EndpointFollowing) => {
+      const totalFollowers = new Intl.NumberFormat("de-DE").format(
         follower.followers.total
-      ),
-    })
+      )
+
+      return {
+        name: follower.name,
+        url: follower.external_urls.spotify,
+        cover: follower.images[1].url,
+        id: follower.id,
+        popularity: follower.popularity,
+        followers: totalFollowers,
+      }
+    }
   )
 
   return formattedFollowing
